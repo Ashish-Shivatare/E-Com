f27198be6bf0e0611237841774e04fd31079428e
Ashish8950 <8967ashish@gmail.com>
2021-01-09 14:44:14 +0530

    Data fetch from URL and Displayed on Page

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from "src/app/common/product";
+import { Dish } from "src/app/common/dish";
 
 @Component({
   selector: 'app-product-list',
@@ -10,11 +11,12 @@ import { Product } from "src/app/common/product";
 export class ProductListComponent implements OnInit {
 
   products:Product[];
+  dishes: Dish[];
   
   constructor(private productService : ProductService) { }
 
   ngOnInit() {
-
+    this.listDishes();
     this.listProducts();
     var products = JSON.stringify(products);
 
@@ -24,10 +26,12 @@ export class ProductListComponent implements OnInit {
       data => {
         this.products = data;
         console.log(data);
-      }
-      
-    );
-      
+      });    
+  }
+
+  listDishes(){
+    this.productService.getDishes().subscribe(
+      (dishes) => (this.dishes = dishes));
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from "../common/product";
 import { map } from "rxjs/operators";
+import { Dish } from "../common/dish";
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,20 @@ import { map } from "rxjs/operators";
 export class ProductService {
 
   private baseUrl='http://localhost:8080/api/products';
+  private baseURL='http://localhost:3000/dishes';
 
   constructor(private httpClient: HttpClient) { }
 
-
   getProductList(): Observable<Product[]>{
-
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
-      
     );
+  }
+
+  
+  getDishes(): Observable<Dish[]> {
+    return this.httpClient
+      .get<Dish[]>(this.baseURL);
   }
 }
 
